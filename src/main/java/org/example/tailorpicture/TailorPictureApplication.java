9994707975c8e1098c1d07fdca56a947d1fb7dcc
wuhao <1589419179@qq.com>
2026-05-29 16:13:05 +0800

    init

---
+++ src/main/java/org/example/tailorpicture/TailorPictureApplication.java
@@ -0,0 +1,14 @@
+package org.example.tailorpicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailorPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TailorPictureApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/org/example/tailorpicture/TailorPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tailorpicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailorPictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
